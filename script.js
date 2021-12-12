2e08d7347fd97b4673541c391f77f206f9eb3ad5
DavitBoo <davidmarras@gmail.com>
2021-12-12 17:39:14 +0100

    update 12-12-2021 - 17-39

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const file = document.getElementById('the-file');
+const 
+    file = document.getElementById('the-file'),
+    searchWordForm = document.querySelector('.search-word'),
+    searchInput = document.getElementById('search-input');
 
+let fileContent = '';
+let fileArray = [];
+let wordObj = {};
+let fullSortedWordArray = [];
 
 
 // =====  get the file and extract the text content
@@ -8,18 +15,87 @@ const getFile = (e) => {
 
     var reader = new FileReader();
     reader.onload = function(){
-        var dataURL = reader.result;    
+        var text = reader.result;   
+        text = text.toLowerCase(); 
         var output = document.getElementById('output');
-        output.value=dataURL;
-        return dataURL;
+        output.value=text;
+        fileContent=text;
+        textToString();
+        arrayCountedWords();
     };
-    // console.log(input.files[0])
     reader.readAsText(input.files[0]);
     
 }
 
 
+// ========= converts the text to words array
+const textToString = () => {
+    fileArray = fileContent.split(/\s+/ );
+    // console.log(fileArray);
+}
 
-const fileString = file.addEventListener('change', getFile);
-console.log(fileString);
+
+// ========= creates an object with all the word and it counts them
+const arrayCountedWords = () => {
+    const fullSortedWordArray = [];
+
+    //vamos a crerar un objeto con las palabras del texto
+    for (let i = 0; i < fileArray.length; i++) {
+        if(wordObj[fileArray[i]]){
+            wordObj[fileArray[i]]++;
+        }else{
+            wordObj[fileArray[i]] = 1;
+        }    
+    }
+
+    // console.log(wordObj);
+
+    // wordArr = Object.entries(wordObj);
+    // console.log(wordArr);
+    upDateList(wordObj);
+    sortObj(Object.entries(wordObj));
+
+}
+
+
+// ==== it shows the word list in the HTML
+const upDateList = wordObj => {
+    const wordList = document.querySelector('.lista-palabras');
+    const stringfyWordObj = JSON.stringify(wordObj)
+    console.log(stringfyWordObj);
+    wordList.textContent = "The list:" + stringfyWordObj;
+}
+
+
+// ======= Object to an array with the values ------------------------------------------------------
+const objToArray = wordObj => {
+
+}
+
+const sortObj = (wordArr) => {
+    console.log(wordArr);
+    wordArr.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    console.log(wordArr)
+    fullSortedWordArray = wordArr;
+}
+
+
+// AHORA QUE TENGO UN OBJETO CON TODAS LAS PALABRAS ESTO ES SUSTITUIBLE y MEJORABLE --------------------------------
+function searchWord(e) {
+    e.preventDefault();
+    let str = searchInput.value;
+    let strArray =  fileArray;
+    for (let i=0; i<strArray.length; i++) {
+        if (strArray[i].match(str)) return console.log(i);
+    }
+    return -1;
+}
+
+
+
+file.addEventListener('change', getFile);
+searchWordForm.addEventListener('submit', searchWord);
+ 
 
